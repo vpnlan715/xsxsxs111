3f523fcc1befa52fbf016dbf178071546fcc045f
vpnlan715 <56340553+vpnlan715@users.noreply.github.com>
2019-11-12 16:22:14 +0300

    nnn

---
+++ js/main.js
@@ -0,0 +1,15 @@
+	$('.slider-thumb').slick({
+		vertical: true, 
+		verticalSwiping: true,
+		adaptiveHeight: true, 
+		dots: true,
+		arrows: true,
+  		infinite: true,
+  		slidesToShow: 1,
+  		slidesToScroll: 1,
+  		adaptiveHeight: true,
+  		zIndex: 9999,
+		prevArrow: '<button type="button" class="slick-prev"><i class="fas fa-long-arrow-alt-up"></i></button>',
+		nextArrow: '<button type="button" class="slick-next"><i class="fas fa-long-arrow-alt-down"></i></button>',   
+   		dotsClass: 'dots-style'
+	});                         
